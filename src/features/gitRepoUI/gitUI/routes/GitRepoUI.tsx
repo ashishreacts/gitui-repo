fd9ac354f7b625cddb4bd4c1bc8efe0adb027052
AJ5154 <ashishjadhav597@gmail.com>
2024-03-27 19:25:44 +0530

    done the open source component

--- src/features/gitRepoUI/gitUI/routes/GitRepoUI.tsx
+++ src/features/gitRepoUI/gitUI/routes/GitRepoUI.tsx
@@ -1,12 +1,21 @@
 import { ContentLayout } from "@/components/Layout";
 import { GitBreadCrumb } from "../components/GitBreadCrumb";
 import { Home } from "../components/Home";
+import { OpenSource } from "../components/OpenSource";
+import { Grid, Stack } from "@mui/material";
 
 export const GitRepoUI = () => {
   return (
     <ContentLayout title="">
       <GitBreadCrumb />
-      <Home />
+      <Grid container marginTop={0.5} spacing={2}>
+        <Grid item xs={12} md={10}>
+          <Stack direction="column" spacing={2}>
+            <Home />
+            <OpenSource />
+          </Stack>
+        </Grid>
+      </Grid>
     </ContentLayout>
   );
 };
