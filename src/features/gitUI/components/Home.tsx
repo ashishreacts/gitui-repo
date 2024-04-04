24e404e9a12d4e1ad5948a5c62c917b5d4c39157
AJ5154 <ashishjadhav597@gmail.com>
2024-04-04 18:15:41 +0530

    done formatting changes

--- src/features/gitUI/components/Home.tsx
+++ src/features/gitUI/components/Home.tsx
@@ -1,5 +1,12 @@
 import { CalendarToday, FolderOpen, PushPin } from "@mui/icons-material";
-import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
@@ -16,32 +23,34 @@ export const Home = () => {
         }
       />
       <CardContent>
-        <Typography>
-          Embarking on the journey of open source contributions has been an
-          exhilarating experience for me. In this blog post, I'll share some
-          highlights of my recent contributions to various projects that...
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item md={2}>
-            <Typography>
-              <CalendarToday sx={{ verticalAlign: "middle" }} />
-              Feb 10,2024
-            </Typography>
+        <Stack direction="column" spacing={2}>
+          <Typography textAlign="justify">
+            Embarking on the journey of open source contributions has been an
+            exhilarating experience for me. In this blog post, I'll share some
+            highlights of my recent contributions to various projects that...
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item md={2}>
+              <Typography>
+                <CalendarToday sx={{ verticalAlign: "middle" }} />
+                Feb 10,2024
+              </Typography>
+            </Grid>
+            <Grid item md={2}>
+              <Typography>
+                <FolderOpen sx={{ verticalAlign: "middle" }} />
+                Open Source
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              md={7}
+              sx={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <PushPin />
+            </Grid>
           </Grid>
-          <Grid item md={2}>
-            <Typography>
-              <FolderOpen sx={{ verticalAlign: "middle" }} />
-              Open Source
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            md={8}
-            sx={{ display: "flex", justifyContent: "flex-end" }}
-          >
-            <PushPin />
-          </Grid>
-        </Grid>
+        </Stack>
       </CardContent>
     </Card>
   );
